f4e4c2f8462da8b2d58812d42a4becd319971701
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-11-27 18:17:53 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 now=$(date +"%d-%m-%Y")
 log_file=/home/install/log_script_install-$now.log
 touch $log_file
+echo "Debut du script" > $log_file
 
 # Premier parametre: MESSAGE
 # Autres parametres: COMMAND
@@ -14,9 +15,9 @@ displayandexec() {
   sh -c "$*" >> $log_file 2>&1
   local ret=$?
   if [ $ret -ne 0 ]; then
-    echo -e "\r\e[0;30m $message  \e[0;31m[ERROR]\e[0m"
+    echo -e "\r\e[0;30m $message                       \e[0;31m[ERROR]\e[0m "
   else
-    echo -e "\r\e[0;30m $message     \e[0;32m[OK]\e[0m"
+    echo -e "\r\e[0;30m $message                       \e[0;32m[OK]\e[0m "
   fi
   return $ret
 }
