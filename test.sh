3db5cc3dc91e4db2bc1c0e8de3d27f714a6eea28
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-11-27 17:43:16 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+now=$(date +"%d-%m-%Y")
+log_file=/home/install/log_script_install-$now.log
+touch $log_file
+
+# Premier parametre: MESSAGE
+# Autres parametres: COMMAND
+displayandexec() {
+  local message=$1
+  echo -n "[En cours] $message"
+  shift
+  echo ">>> $*" >> $log_file 2>&1
+  sh -c "$*" >> $log_file 2>&1
+  local ret=$?
+  if [ $ret -ne 0 ]; then
+    echo -e "\r\e[0;31m   [ERROR]\e[0m $message"
+  else
+    echo -e "\r\e[0;32m      [OK]\e[0m $message"
+  fi
+  return $ret
+}
+#exemple :
+displayandexec "Installation de mediainfo" "apt-get install -y mediainfo"
